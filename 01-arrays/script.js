10f712a7651db9b1dfc0eb8ee2986884776b244e
AlbertoMameli <mamelialberto2@gmail.com>
2025-02-22 13:15:31 +0100

    feat: inizio essercizio

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -4,16 +4,25 @@ const teachers = ["Nathan", "Ed", "Fabio", "Phil", "Carlo", "Lewis", "Luca"]; //
 // [Nome insengante] è l'insegnante in posizione [i]
 for (let i = 0; i < teachers.length; i++){
 
-    const curTeachers = teachers[i]
+    const curTeachers = teachers[i];
 
     console.log(i,curTeachers);
     
 }
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
-const fourthTeacher = null;
+
+const fourthTeacher = teachers [4];
+console.log(fourthTeacher);
+
+    
+
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
+teachers [5] = 'Patrick';
+console.log(teachers);
+
+
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = null;
@@ -22,8 +31,14 @@ const lastTeacher = null;
 const firstTeacher = null;
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+teachers.push("Vanessa");
+
+
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
+teachers.unshift("Sarah")
+console.log(teachers);
+
 
 // 7. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
